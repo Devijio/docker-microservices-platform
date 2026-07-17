aa689564e90a0d69ae4318ba68fabae9c0e3e329
Chapati Umadevi <89294520+Devijio@users.noreply.github.com>
2026-07-18 00:00:56 +0530

    Create app.py

---
+++ user-service/app.py
@@ -0,0 +1,4 @@
+from http.server import HTTPServer,BaseHTTPRequestHandler
+class H(BaseHTTPRequestHandler):
+    def do_GET(self): self.send_response(200); self.end_headers(); self.wfile.write(b'OK')
+HTTPServer(('',8000),H).serve_forever()
